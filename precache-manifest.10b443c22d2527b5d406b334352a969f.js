f6c59a197962055448c2129ecc44f2a2863263f9
Thilak238 <thilakchakravarthi1@gmail.com>
2020-09-25 19:57:36 +0530

    Updates

---
+++ precache-manifest.10b443c22d2527b5d406b334352a969f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ade806e118d4250807d2f7d0694589d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c69eb5f9e4fa4e8ebb73",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5e7832321e51d77596f3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c69eb5f9e4fa4e8ebb73",
+    "url": "/static/js/2.a72d7f0c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a72d7f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7832321e51d77596f3",
+    "url": "/static/js/main.d0516f6e.chunk.js"
+  },
+  {
+    "revision": "944349a3d41aa2d318c9",
+    "url": "/static/js/runtime-main.e7dfe56e.js"
+  }
+]);
